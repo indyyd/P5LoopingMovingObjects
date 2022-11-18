093559aed5a3b13dddea46a3b370e5ae8fc40430
Indy <116882977+indyyd@users.noreply.github.com>
2022-11-18 13:00:42 +0000

    Add files via upload
    basic ball moving

--- js/sketch.js
+++ js/sketch.js
@@ -1,11 +1,28 @@
+let xPos, yPos, xDir, yDir;
+
 function setup() {
-    var canvas = createCanvas(800, 800);
+    var canvas = createCanvas(400, 400);
     canvas.parent("p5container");
+    xPos = width/2;
+    yPos = height/2;
+    xDir = 1;
+    yDir = 1;
 }
 
 function draw() {
     background(220);
+    fill(0);
+    circle(xPos, yPos, 50);
 
-    fill(255, 255, 255);
-    ellipse(400, 400, 200);
+    xPos = xPos + xDir;
+    
+    if (xPos+25 > width || xPos < 0) {
+        xDir *= -1;
+    }
+
+    yPos = yPos + yDir;
+
+    if (yPos+25 > height || yPos < 0) {
+        yDir *= -1;
+    }
 }
